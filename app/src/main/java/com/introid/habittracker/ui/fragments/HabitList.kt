b435188b0913a4f4fe0136ab4e42021ec684aebf
prakashRanjan19 <prakash.r19500@gmail.com>
2020-10-06 10:15:01 +0530

    Showing Habit List

--- app/src/main/java/com/introid/habittracker/ui/fragments/HabitList.kt
+++ app/src/main/java/com/introid/habittracker/ui/fragments/HabitList.kt
@@ -2,22 +2,81 @@ package com.introid.habittracker.ui.fragments
 
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.introid.habittracker.R
+import com.introid.habittracker.adapters.HabitListAdapter
+import com.introid.habittracker.data.models.Habit
+import com.introid.habittracker.ui.viewmodels.HabitViewModel
 import kotlinx.android.synthetic.main.fragment_habit_list.*
 
 
 class HabitList : Fragment(R.layout.fragment_habit_list) {
 
+    private lateinit var habitList: List<Habit>
+    private lateinit var habitViewModel: HabitViewModel
+    private lateinit var adapter: HabitListAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fab_add.setOnClickListener{
+        // adapters
+        adapter = HabitListAdapter()
+        rv_habits.adapter = adapter
+        rv_habits.layoutManager = LinearLayoutManager(context)
+
+        //view model
+        viewModels()
+
+        fab_add.setOnClickListener {
             findNavController().navigate(R.id.action_habitList_to_createHabitItem)
         }
 
+        //Show the options menu in this fragment
+        setHasOptionsMenu(true)
+
+
+        swipeToRefresh.setOnRefreshListener {
+            adapter.setData(habitList)
+            swipeToRefresh.isRefreshing = false
+        }
+
     }
+
+    private fun viewModels() {
+        habitViewModel = ViewModelProvider(this).get(HabitViewModel::class.java)
+
+        habitViewModel.getAllHabits.observe(viewLifecycleOwner, Observer {
+            adapter.setData(it)
+            habitList = it
+
+            if (it.isEmpty()) {
+                rv_habits.visibility = View.GONE
+                tv_emptyView.visibility = View.VISIBLE
+            } else {
+                rv_habits.visibility = View.VISIBLE
+                tv_emptyView.visibility = View.GONE
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_delete -> habitViewModel.deleteAllHabits()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
